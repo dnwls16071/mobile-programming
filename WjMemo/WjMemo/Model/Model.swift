68da2b80261a3f9758d9ceea21439016233a57d6
dnwls16071 <106802375+dnwls16071@users.noreply.github.com>
2022-06-10 17:33:29 +0900

    Add files via upload

---
+++ WjMemo/WjMemo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  WjMemo
+//
+//  Created by 203a21 on 2022/06/09.
+//
+
+import Foundation
+
+
+//class Memo  {
+//    var content: String
+//    var insertDate: Date
+//
+//    init(content: String)   {
+//        self.content = content
+//        insertDate = Date()
+//    }
+//
+//    static var dummyMemoList = [
+//        Memo(content: "모바일 프로그래밍 기말고사 일정 : 6/17"),
+//        Memo(content: "12,13,14장 + 기말고사 프로젝트 제출하기")]
+//}
